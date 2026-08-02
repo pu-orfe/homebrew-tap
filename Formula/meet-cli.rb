6f39eae14bd29b718dd0c450cfeced5a1a2a141a
Michael Bino <bino@princeton.edu>
2026-08-02 10:31:37 -0400

    meet-cli 0.1.4
    Adds `meet agenda show` and `agenda-link create --agenda-name`. Released in
    step with the /cli page that documents them.

--- Formula/meet-cli.rb
+++ Formula/meet-cli.rb
@@ -11,9 +11,9 @@ class MeetCli < Formula
   # previously tracked the cli-companion-phase1 feature branch, which vanished
   # when it merged and broke `brew install` outright.
   url "https://github.com/pu-orfe/meet.git",
-      tag:      "meet-cli-v0.1.3",
-      revision: "fab872d41741ebbd4c72b631ab068367b19c0891"
-  version "0.1.3"
+      tag:      "meet-cli-v0.1.4",
+      revision: "0511db97ed1d773f17ebe365eecc4e932da3d6ad"
+  version "0.1.4"
   license "MIT"
 
   depends_on "python@3.12"
